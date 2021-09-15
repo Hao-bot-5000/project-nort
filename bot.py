7b7684659075a6d05fdd0f4d9b6cb198b205cbcd
htran25 <htran25@ucsc.edu>
2021-09-15 16:40:48 -0700

    initial commit

---
+++ bot.py
@@ -0,0 +1,24 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
