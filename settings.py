1d68542fa99dfd1009c5d2db867a7d2ff0563027
htran25 <htran25@ucsc.edu>
2021-09-16 19:29:34 -0700

    updated 'Playing' tag

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ COMMAND_PREFIX = "!nort "
 BOT_TOKEN = TOKEN
 
 # The now playing game. Set this to anything false-y ("", None) to disable it
-NOW_PLAYING = COMMAND_PREFIX + "commands"
+NOW_PLAYING = "FF14 with the Norters" # COMMAND_PREFIX + "commands"
 
 # Base directory. Feel free to use it if you want.
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
