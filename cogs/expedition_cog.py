1bf0d09176d1e24af7d5dadc24292c72e6997632
nhantrungha <nhantrungha@gmail.com>
2021-09-27 14:09:38 -0700

    reformated expedition code

--- cogs/expedition_cog.py
+++ cogs/expedition_cog.py
@@ -20,7 +20,7 @@ class ExpeditionCog(BaseCog):
     @commands.command(
         aliases=["exp", "quest"],
         brief="Go on expedition",
-        description="Starts an expedition to gather NortCoins after a certain time"
+        description="Starts an expedition to gather NortCoins (default 1 hour) Usage: !nort exp 3h, 6h, 12h"
     )
     @commands.guild_only()
     async def expedition(self, ctx, *args):
@@ -33,26 +33,46 @@ class ExpeditionCog(BaseCog):
         yc_members_data = guild_data.get("yc_members", {})
 
         author_data = yc_members_data.get(author_id, {})
+        time = ''
+        if len(args) > 1:
+            await ctx.send("Too many arguments. Please enter: 3h, 6h, 12h or leave blank for 1h")
+            return
+        if 'help' in args:
+            await ctx.send("Usage: !nort exp __ (blank for 1h, 3h, 6h, 12h)")
+            return
+
         if author_data["on_expedition"] == 0:
             author_data["on_expedition"] = 1
+            if len(args) == 0:
+                time = '1h'
+            time = str(args[0])
+            asyncio.create_task(self.inner(ctx, author_data, json_data, time))
             await ctx.send("Expedition started!")
-            asyncio.create_task(self.inner(ctx, author_data, json_data))
             await set_json_data(JSON_DATA_PATH, json_data)
         else:
             await ctx.send("Currently on expedition!")
 
-    async def inner(self, ctx, author_data, json_data):
-
-        await asyncio.sleep(10)
+    async def inner(self, ctx, author_data, json_data, time):
+        if str(time) == '3h':
+            await asyncio.sleep(5)
+            author_data["nort_coins"] += 3
+        elif str(time) == '6h':
+            await asyncio.sleep(10)
+            author_data["nort_coins"] += 5
+        elif str(time) == '12h':
+            await asyncio.sleep(15)
+            author_data["nort_coins"] += 100
+        else:
+            await asyncio.sleep(2)
+            author_data["nort_coins"] += 1000
         await ctx.send("Expedition completed!")
-        author_data["nort_coins"] += 100
         author_data["on_expedition"] = 0
         await set_json_data(JSON_DATA_PATH, json_data)
 
     ### Daily Claim Command ###
     @commands.command(
-        brief="Claim daily YashCoins",
-        description="Claim daily YashCoins"
+        brief="Claim daily NortCoins",
+        description="Claim daily NortCoins"
     )
     @commands.guild_only()
     async def daily(self, ctx, *args):
